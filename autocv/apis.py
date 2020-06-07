eaa69f65498bf2531ef32fe22b69d4aa001215fe
poldrack <poldrack@gmail.com>
2020-06-07 10:30:05 -0700

    move functions in to researcher class

--- autocv/apis.py
+++ autocv/apis.py
@@ -2,52 +2,9 @@
 functions to access various APIs
 """
 
-import requests
-from Bio import Entrez
 from crossref.restful import Works
 import pypatent
 import scholarly
-
-
-def get_orcid_data(id):
-    resp = requests.get("http://pub.orcid.org/%s" % id,
-                        headers={'Accept': 'application/orcid+json'})
-    orcid_data = resp.json()
-    return(orcid_data)
-
-
-# get DOIs for pubs in orcid
-def get_orcid_dois(orcid_data):
-    dois = []
-    for g in orcid_data['activities-summary']['works']['group']:
-        for p in g['work-summary']:
-            doi = None
-            for eid in p['external-ids']['external-id']:
-                if eid['external-id-type'] == 'doi':
-                    doi = eid['external-id-value'].replace('http://dx.doi.org/', '')
-            if doi is not None:
-                dois.append(doi.lower())
-    return(list(set(dois)))
-
-
-def get_pubmed_records(query, email=None):
-    if email is not None:
-        Entrez.email = email
-        print(f'using {email} for Entrez service')
-    print('searching for', query)
-    retmax = 1000
-    handle = Entrez.esearch(db="pubmed", retmax=retmax, term=query)
-    record = Entrez.read(handle)
-    handle.close()
-    pmids = [int(i) for i in record['IdList']]
-    print('found %d matches' % len(pmids))
-
-    # load full records
-    handle = Entrez.efetch(db="pubmed", id=",".join(['%d' % i for i in pmids]),
-                           retmax=retmax, retmode="xml")
-    records = Entrez.read(handle)
-    print('retrieved %d full pubmed records' % len(records['PubmedArticle']))
-    return(records)
 
 
 def get_crossref_records(dois):
